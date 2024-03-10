ab0b2a2be5b55c07667607b1565c90913a2daf4e
Joseph Goff <josephgoff@josephs-laptop.lan>
2024-03-10 14:10:16 -0700

    3/10 2PM changed server url

--- store/src/config.js
+++ store/src/config.js
@@ -1,12 +1,11 @@
 let localServer = false
 
 let serverURL = "http://localhost:8080"
-let frontendURL = "http://localhost:3000"
+let frontendURL = "http://localhost:3000"  
 
 if (!localServer) {serverURL = "https://store.expertsoncall.online"}
 if (process.env.NODE_ENV === 'production') {
     // serverURL = "http://ec2-18-227-140-97.us-east-2.compute.amazonaws.com:8080"
-    serverURL = "https://store.expertsoncall.online"
     frontendURL = "https://opendreamdesign.com"
 }
 
